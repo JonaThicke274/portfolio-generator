afdfd411031438ae04d8442184faafb15a2217ac
JonaThicke274 <lipatajonathan274@gmail.com>
2022-09-20 19:14:42 -0400

    init npm, installed inquirer, init .gitignore

--- app.js
+++ app.js
@@ -1,27 +1,35 @@
-const fs = require("fs");
+const inquirer = require("inquirer");
+// const fs = require("fs");
+// const generatePage = require("./src/page-template.js");
 
-const generatePage = require("./src/page-template.js");
+// const pageHTML = generatePage(name, github);
 
-const profileDataArgs = process.argv.slice(2);
-
-const [name, github] = profileDataArgs;
-
-// const printProfileData = profileDataArr => {
-//     // This...
-//     for (let i = 0; i < profileDataArr.length; i++) {
-//         console.log(profileDataArr[i]);
-//     }
+// // const printProfileData = profileDataArr => {
+// //     // This...
+// //     for (let i = 0; i < profileDataArr.length; i++) {
+// //         console.log(profileDataArr[i]);
+// //     }
     
-//     console.log("=============");
+// //     console.log("=============");
 
-//     // Is the same as this...
-//     profileDataArr.forEach((profileItem) => console.log(profileItem));
-// };
+// //     // Is the same as this...
+// //     profileDataArr.forEach((profileItem) => console.log(profileItem));
+// // };
 
-// printProfileData(profileDataArgs);
+// // printProfileData(profileDataArgs);
 
-fs.writeFile("index.html", generatePage(name,github), err => {
-    if (err) throw new Error(err);
+// fs.writeFile("./index.html", pageHTML, err => {
+//     if (err) throw err;
 
-    console.log("Portfolio complete! Checkout index.html to see the output!");
-});
+//     console.log("Portfolio complete! Checkout index.html to see the output!");
+// });
+
+inquirer
+    .prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "What is your name?"
+        }
+    ])
+    .then(answers => console.log(answers));
